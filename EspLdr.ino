a75e10fd43f429280371991f5ef8f89d6332a64b
Orivaldo Santana Jr <orivaldo@gmail.com>
2018-04-13 04:42:14 -0300

    Update EspLdr.ino

--- EspLdr.ino
+++ EspLdr.ino
@@ -5,9 +5,6 @@ void setup() {
 void loop() {
 
   int sensorValue = analogRead(A0);   // leitura da porta analogica no pino A0
-
-  float voltage = sensorValue * (5.0 / 1023.0);   // converte a leitura para a voltagem sendo passda pelo resistor (which goes from 0 - 1023) to a voltage (0 - 3.3V)
-
   Serial.println(sensorValue);   // Plotar o valor da leitura no serial
 
 }
